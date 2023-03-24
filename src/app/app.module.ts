d9c28415da5eebdb0dc596050065ef1087011b8a
DJR <djr@gmail.com>
2023-03-24 17:06:53 -0600

    login aceptable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,10 @@ import { environment } from 'src/environments/environment';
 import { FooterComponent } from './components/footer/footer.component';
 import { ChatComponent } from './components/chat/chat.component';
 import { FormsModule } from '@angular/forms';
+import { ListaUsuariosComponent } from './components/lista-usuarios/lista-usuarios.component';
+import { LoginComponent } from './pages/login/login.component';
+import { MensajesComponent } from './pages/mensajes/mensajes.component';
+import { AppRoutingModule } from './app-routing.module';
 const config: SocketIoConfig = { url: environment.wsUrl, options: {} };
 
 
@@ -14,10 +18,14 @@ const config: SocketIoConfig = { url: environment.wsUrl, options: {} };
   declarations: [
     AppComponent,
     FooterComponent,
-    ChatComponent
+    ChatComponent,
+    ListaUsuariosComponent,
+    LoginComponent,
+    MensajesComponent
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     SocketIoModule.forRoot(config),
     FormsModule
   ],
