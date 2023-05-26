8b77d0e6c95e720464c21e86f3d143270cb62158
DJR <djr@gmail.com>
2023-05-26 10:08:47 -0600

    cambios

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,5 @@
 export const environment = {
-    wsUrl: 'https://chat-djr.herokuapp.com',
+    // wsUrl: 'https://chat-djr.herokuapp.com',
+    wsUrl: 'https://quickstart-1598129806269.uc.r.appspot.com/',
+    
 };
